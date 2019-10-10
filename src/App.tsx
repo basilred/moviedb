59c1277413394b835b5b1c3eb8d5062613901847
Vassily Krasnov <mykrasnov@gmail.com>
2019-10-10 23:39:32 +0300

    Remove logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import axios from 'axios';
@@ -16,7 +15,6 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>

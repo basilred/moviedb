1c4f95a855077716511ce2204baf9396264f35f8
Vassily Krasnov <mykrasnov@gmail.com>
2019-10-13 22:51:49 +0300

    Add error & loading states

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,61 @@ import axios from 'axios';
 import ICurrency from './interfaces/ICurrency';
 import CurrencyList from './components/CurrencyList';
 
+interface IState {
+  data: ICurrency[],
+  loading: boolean;
+  errored: boolean;
+}
+
 class App extends React.Component {
-  state: { data: ICurrency[] };
+  state: IState;
 
   constructor(props: Readonly<{}>) {
     super(props);
     this.state = {
-      data: []
+      data: [],
+      loading: true,
+      errored: false,
     }
   }
 
   componentDidMount() {
     axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
       .then(response => {
-        const { bpi } = response.data;
-        const result = [];
-
-        for (const item in bpi) {
-          if (bpi.hasOwnProperty(item)) {
-            const element = bpi[item];
-            result.push(element);
-          }
-        }
-
-        this.setState({ data: result });
+        this.setState({ data: this.toCurrencyArray(response.data.bpi) });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ errored: true });
+      })
+      .finally(() => {
+        this.setState({ loading: false });
       });
   }
 
+  toCurrencyArray(bpi: any) {
+    const result = [];
+
+    for (const item in bpi) {
+      if (bpi.hasOwnProperty(item)) {
+        const element = bpi[item];
+        result.push(element);
+      }
+    }
+
+    return result;
+  }
+
   render() {
+    const {loading, errored} = this.state;
+    const bitcoinBlock = loading ? <p>Loading…</p> : <CurrencyList list={this.state.data} />;
+    const errorMsg = <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>;
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Bitcoin</h1>
-          <CurrencyList list={this.state.data} />
+          <h1>Bitcoin Price Index</h1>
+          { errored ? errorMsg : bitcoinBlock }
         </header>
       </div>
     );

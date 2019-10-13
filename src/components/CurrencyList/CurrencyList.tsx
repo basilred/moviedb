41a2f3f075a395122a1e94b0fb1dc6b42ac2c0f1
Vassily Krasnov <mykrasnov@gmail.com>
2019-10-14 00:55:21 +0300

    Move currency item into a separate component

--- src/components/CurrencyList/CurrencyList.tsx
+++ src/components/CurrencyList/CurrencyList.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import ICurrency from '../../interfaces/ICurrency';
 import './CurrencyList.css';
 
+const CurrencyItem = ({ item }: { item: ICurrency }) => {
+  return (
+    <li>
+      {item.description}{' '}
+      <span className="sign" dangerouslySetInnerHTML={{ __html: item.symbol }} />
+      {item.rate_float.toFixed(2)}
+    </li>
+  );
+};
+
 const CurrencyList = ({ list }: { list: ICurrency[] }) => {
   return (
     <ul className="CurrencyList">
-      {list.map(item => (
-        <li key={item.code}>
-          {item.description}{' '}
-          <span className="sign" dangerouslySetInnerHTML={{ __html: item.symbol }} />
-          {item.rate_float.toFixed(2)}
-        </li>
-      ))}
+      {list.map(item => <CurrencyItem key={item.code} item={item} />)}
     </ul>
   );
 };
